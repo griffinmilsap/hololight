1a4eaa17e824e78b20989d2b9120a45cde66ecb5
Griffin W. Milsap <griffin.milsap@jhuapl.edu>
2021-11-29 11:19:52 -0500

    openbci module instantiated

--- hololight/bci.py
+++ hololight/bci.py
@@ -1,14 +1,48 @@
 import ezmsg as ez
 
+from ezbci.openbci.openbci import OpenBCISourceSettings
+
 from .hololightsystem import HololightSystem, HololightSystemSettings
 
 if __name__ == "__main__":
     import argparse
 
-    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description = 'Hololight ARBCI Lightbulb Demonstration'
+    )
 
+    parser.add_argument(
+        '--device',
+        type = str,
+        help = 'Serial port to pull data from',
+        default = 'simulator'
+    )
 
+    parser.add_argument(
+        '--blocksize',
+        type = int,
+        help = 'Sample block size @ 500 Hz',
+        default = 50
+    )
 
-    settings = HololightSystemSettings()
+    parser.add_argument(
+        '--poll',
+        type = float,
+        help = 'Poll Rate (Hz). 0 for auto-config',
+        default = 0.0
+    )
+
+    args = parser.parse_args()
+
+    openbcisource_settings = OpenBCISourceSettings(
+        device = args.device,
+        blocksize = args.blocksize,
+        poll_rate = None if args.poll <= 0 else args.poll
+    )
+
+    settings = HololightSystemSettings(
+        openbcisource_settings = openbcisource_settings
+    )
+
     system = HololightSystem( settings )
     ez.run_system( system )
--- hololight/hololightsystem.py
+++ hololight/hololightsystem.py
@@ -1,8 +1,34 @@
+from dataclasses import field
 import ezmsg as ez
 
+from ezbci.openbci.openbci import OpenBCISource, OpenBCISourceSettings
+
+from typing import Any
+
+class DebugPrint( ez.Unit ):
+
+    INPUT = ez.InputStream( Any )
+
+    @ez.subscriber( INPUT )
+    async def on_message( self, message: Any ) -> None:
+         print( message )
+
 class HololightSystemSettings( ez.Settings ):
-    ...
+    openbcisource_settings: OpenBCISourceSettings
 
 class HololightSystem( ez.System ):
-    ...
+
+    SETTINGS: HololightSystemSettings
+
+    SOURCE = OpenBCISource()
+    DEBUG = DebugPrint()
+
+    def configure( self ) -> None:
+        self.SOURCE.apply_settings( self.SETTINGS.openbcisource_settings )
+
+    def network( self ) -> ez.NetworkDefinition:
+        return ( 
+            ( self.SOURCE.OUTPUT_SIGNAL, self.DEBUG.INPUT ),
+        )
+
 
