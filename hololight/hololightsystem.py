6e41def82121419d05a6516efa05b856841ed52f
Hannah Gooden <hannah.gooden@jhuapl.edu>
2022-05-26 20:50:04 -0400

    Add websocket

--- hololight/hololightsystem.py
+++ hololight/hololightsystem.py
@@ -2,6 +2,8 @@ from dataclasses import field
 import ezmsg.core as ez
 
 from ezmsg.eeg.openbci import OpenBCISource, OpenBCISourceSettings
+from ezmsg.websocket import WebsocketServer, WebsocketSettings
+from ezmsg.util.messagelogger import MessageLogger, MessageLoggerSettings
 
 from .modeltraining import ModelTraining, ModelTrainingSettings
 from .preprocessing import Preprocessing, PreprocessingSettings
@@ -41,13 +43,25 @@ class HololightSystem( ez.System ):
 
     DEBUG = DebugPrint()
 
+    WS_SERVER = WebsocketServer()
+    WS_MESSAGES = MessageLogger() 
+
     def configure( self ) -> None:
         self.SOURCE.apply_settings( self.SETTINGS.openbcisource_settings )
         self.PREPROC.apply_settings( self.SETTINGS.preprocessing_settings )
         self.DECODER.apply_settings( self.SETTINGS.decoder_settings )
         self.TRAINING.apply_settings( self.SETTINGS.modeltraining_settings )
         self.HUE.apply_settings( self.SETTINGS.huedemo_settings )
+        self.WS_SERVER.apply_settings( WebsocketSettings(
+            host="127.0.0.1",
+            port="8082"
+        ) )
+        self.WS_MESSAGES.apply_settings( MessageLoggerSettings(
+            output="websocket_messages.json"
+        ))
 
+    # TODO: figure out where websocket input should go and add it to relevant module
+    #       for now we send to message logger :)
     def network( self ) -> ez.NetworkDefinition:
         return ( 
             ( self.SOURCE.OUTPUT_SIGNAL, self.PREPROC.INPUT_SIGNAL ),
@@ -57,6 +71,8 @@ class HololightSystem( ez.System ):
             # ( self.DECODER.OUTPUT_DECODE, self.DEBUG.INPUT ),
             ( self.DECODER.OUTPUT_DECODE, self.HUE.INPUT_DECODE ),
             ( self.PREPROC.OUTPUT_SIGNAL, self.TRAINING.INPUT_SIGNAL ),
+
+            ( self.WS_SERVER.OUTPUT, self.WS_MESSAGES.INPUT_MESSAGE )
         )
 
     def process_components( self ) -> Tuple[ ez.Component, ... ]:
