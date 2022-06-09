9de8cf685c0d6a8fe1997a92693d626c03684514
Hannah Gooden <hannah.gooden@jhuapl.edu>
2022-06-09 14:26:27 -0400

    Modify hue.py to support multiple lightbulbs

--- examples/test_hue.py
+++ examples/test_hue.py
@@ -3,9 +3,17 @@ import ezmsg.core as ez
 import asyncio
 import numpy as np
 
-from ..hololight.shallowfbcspdecoder import DecoderOutput 
-from ..hololight.hue import HueDemo, HueDemoSettings
-from ..hololight.stamped_websocket_server import StampedTextMessage
+from hololight.shallowfbcspdecoder import DecoderOutput
+from hololight.hue import HueDemo, HueDemoSettings
+from hololight.stamped_websocket_server import StampedTextMessage
+
+'''
+Generates dummy websocket messages to simulate a hololens and dummy decode
+messages to simulate input from BCI signals. Pipes them to hue.py.
+
+Expected behavior: hue.py will simulate two lightbulbs. The second will 
+blink on and off.
+'''
 
 class GenerateMessages( ez.Unit ):
   OUTPUT = ez.OutputStream(StampedTextMessage)
@@ -26,7 +34,7 @@ class GenerateDecode( ez.Unit ):
     for i in range(0, 10000):
       await asyncio.sleep(.1)
       yield ( self.OUTPUT, DecoderOutput(
-        output = np.array([0,1])
+        output = np.array([i%2,(i+1)%2])
       ))
 
 class HoloTestSystem( ez.System ):
@@ -35,10 +43,17 @@ class HoloTestSystem( ez.System ):
   MESSAGES = GenerateMessages()
   DECODE = GenerateDecode()
 
-  # def configure( self ) -> None:
+  def configure( self ) -> None:
+    self.HUE.apply_settings( HueDemoSettings(
+      num_lights = 2
+    ))
 
   def network( self ) -> ez.NetworkDefinition:
     return (
       (self.MESSAGES.OUTPUT, self.HUE.INPUT_HOLOLENS),
       (self.DECODE.OUTPUT, self.HUE.INPUT_DECODE)
     )
+
+if __name__ == "__main__":
+  system = HoloTestSystem()
+  ez.run_system(system)
--- hololight/hue.py
+++ hololight/hue.py
@@ -1,5 +1,6 @@
 import ezmsg.core as ez
 import numpy as np
+import time
 
 from phue import Bridge
 
@@ -8,17 +9,21 @@ from .stamped_websocket_server import StampedTextMessage
 
 from typing import (
     Optional,
-    ByteString
+    List
 )
+
+ON = True
+OFF = False
 
 class HueDemoSettings( ez.Settings ):
     trigger_class: int = 1
     trigger_thresh: float = 0.9
     bridge_host: Optional[ str ] = None
+    num_lights: Optional[ int ] = None
 
 # Turn lights_on into a vector of bools
 class HueDemoState( ez.State ):
-    lights_on: bool = False
+    lights_on: List[ bool ] = None
     decode_class: Optional[ int ] = None
     bridge: Optional[ Bridge ] = None
     hololens_state: Optional[ StampedTextMessage ] = None
@@ -36,27 +41,46 @@ class HueDemo( ez.Unit ):
             # Set lights to initial state
             self.STATE.bridge = Bridge( self.SETTINGS.bridge_host )
             self.STATE.bridge.connect()
+            if not self.SETTINGS.num_lights:
+                self.SETTINGS.num_lights = len(self.STATE.bridge.lights)
+            self.STATE.lights_on = [OFF for i in range(self.SETTINGS.num_lights)]
+            self.set_all_lights( OFF )
 
-            self.set_lights( self.STATE.lights_on )
+        else:
+            print("No bridge detected.", self.SETTINGS.num_lights, "lights will be simulated.")
+            self.STATE.lights_on = [ OFF for i in range(self.SETTINGS.num_lights) ]
 
-    def set_lights( self, on: bool ):
-        print( 'Set lights', 'on' if on else 'off' )
+    def set_all_lights( self, on: bool ):
+        for i, light in enumerate(self.STATE.lights_on):
+            self.set_light(i, on)
+
+    def set_light( self, i: int, on: bool ):
+        print( 'Set light', i, 'on' if on else 'off' )
+        self.STATE.lights_on[i] = on
         if self.STATE.bridge:
-            for light in self.STATE.bridge.lights:
-                if light.reachable:
-                    light.on = on
+            light = self.STATE.bridge.lights[i]
+            if light.reachable:
+                light.on = on
+    
+    def flip_light( self, i: int ):
+        current_state = self.STATE.lights_on[i]
+        print( 'Set light', i, 'off' if current_state else 'on' )
+        self.STATE.lights_on[i] = not self.STATE.lights_on[i]
+        if self.STATE.bridge:
+            light = self.STATE.bridge.lights[i]
+            if light.reachable:
+                light.on = not light.on
+
 
     # Post most recently received message to state
     @ez.subscriber( INPUT_HOLOLENS )
     async def on_hololens_input( self, hololens_message: StampedTextMessage ) -> None:
-        print("hololens")
         self.STATE.hololens_state = hololens_message
 
     # If timestamp on ws message in state is older than a half second, don't use it
     # If timestamp is recent enough, turn light on
     @ez.subscriber( INPUT_DECODE )
     async def on_decode( self, decode: DecoderOutput ) -> None:
-        print("decode")
         probs = np.exp( decode.output )
         cur_class = probs.argmax()
         cur_prob = probs[ cur_class ]
@@ -71,8 +95,11 @@ class HueDemo( ez.Unit ):
                 self.STATE.decode_class = cur_class
 
                 if cur_class == self.SETTINGS.trigger_class:
-                    self.STATE.lights_on = not self.STATE.lights_on
-                    self.set_lights( self.STATE.lights_on )
+
+                    ws_message = self.STATE.hololens_state
+                    ws_message_age = time.time() - ws_message._timestamp
+                    if ws_message_age < 0.5:
+                        self.flip_light(int(ws_message.text))
 
     @ez.main
     def dummy( self ):
