4832ec4eb5bc2d5aca920ae061b2cbb544960406
Griffin W. Milsap <griffin.milsap@jhuapl.edu>
2022-07-16 11:50:05 -0400

    significantly better and more capable sampler implementation

--- hololight/sampler.py
+++ hololight/sampler.py
@@ -1,6 +1,5 @@
-from dataclasses import replace
+from dataclasses import dataclass, replace, field
 import logging
-from multiprocessing.sharedctypes import Value
 
 import ezmsg.core as ez
 import numpy as np
@@ -8,24 +7,28 @@ import numpy as np
 from ezmsg.util.stampedmessage import StampedMessage
 from ezmsg.sigproc.timeseriesmessage import TimeSeriesMessage
 
-from typing import Optional, Any, Tuple
+from typing import Optional, Any, Tuple, List
 
 logger = logging.getLogger( __name__ )
 
-class TriggerInfo( StampedMessage ):
-    trigger: Any
+class SampleTriggerMessage( StampedMessage ):
+    period: Tuple[ float, float ]
+    value: Any = None
+
+@dataclass
+class TriggerInfo:
+    msg: SampleTriggerMessage
+    offset: int
 
 class SampleMessage( StampedMessage ):
-    trigger_info: TriggerInfo
+    trigger: SampleTriggerMessage
     sample: TimeSeriesMessage
-    time_offset: float
 
 class SamplerSettings( ez.Settings ):
-    sample_per: Tuple[ float, float ] # seconds
+    buffer_dur: float
 
 class SamplerState( ez.State ):
-    trigger_info: Optional[ TriggerInfo ] = None # sampling if trigger is not None
-    trigger_offset: Optional[ int ] = None
+    triggers: List[ TriggerInfo ] = field( default_factory = list )
     last_msg: Optional[ TimeSeriesMessage ] = None
     buffer: Optional[ np.ndarray ] = None
 
@@ -33,27 +36,41 @@ class Sampler( ez.Unit ):
     SETTINGS: SamplerSettings
     STATE: SamplerState
 
-    INPUT_TRIGGER = ez.InputStream( Any )
+    INPUT_TRIGGER = ez.InputStream( SampleTriggerMessage )
     INPUT_SIGNAL = ez.OutputStream( TimeSeriesMessage )
     OUTPUT_SAMPLE = ez.OutputStream( SampleMessage )
 
-    def initialize( self ) -> None:
-        if self.SETTINGS.sample_per[1] <= self.SETTINGS.sample_per[0]:
-            raise ValueError( f'Invalid sample period: {self.SETTINGS.sample_per[1]=} <= {self.SETTINGS.sample_per[0]=}' )
-
     @ez.subscriber( INPUT_TRIGGER )
-    async def on_trigger( self, msg: Any ) -> None:
-        # TODO: It is probably possible to adapt this code to 
-        # allow multiple overlapping sample acquisitions
-        if self.STATE.trigger_info is None:
-            if self.STATE.last_msg is not None:
-                # Do what we can with the wall clock to determine sample alignment
-                self.STATE.trigger_info = TriggerInfo( trigger = msg )
-                wall_delta = self.STATE.trigger_info._timestamp
-                wall_delta -= self.STATE.last_msg._timestamp
-                self.STATE.trigger_offset = int( wall_delta * self.STATE.last_msg.fs )
-            else: logger.warn( 'Sampling failed: no signal to sample yet' )
-        else: logger.warn( 'Sampling failed: already sampling' )
+    async def on_trigger( self, msg: SampleTriggerMessage ) -> None:
+        if self.STATE.last_msg is not None:
+            fs = self.STATE.last_msg.fs
+
+            # Check that period is valid
+            start_offset = int( msg.period[0] * fs )
+            stop_offset = int( msg.period[1] * fs )
+            if ( stop_offset - start_offset ) <= 0:
+                logger.warn( f'Sampling failed: invalid period requested' )
+                return
+
+            # Check that period is compatible with buffer duration
+            max_buf_len = int( self.SETTINGS.buffer_dur * fs )
+            req_buf_len = int( ( msg.period[1] - msg.period[0] ) * fs )
+            if req_buf_len >= max_buf_len:
+                logger.warn( f'Sampling failed: {msg.period=} >= {self.SETTINGS.buffer_dur=}' )
+                return
+
+            # Do what we can with the wall clock to determine sample alignment
+            wall_delta = msg._timestamp - self.STATE.last_msg._timestamp
+            offset = int( wall_delta * fs )
+
+            # Check that current buffer accumulation allows for offset - period start
+            if -min( offset + start_offset, 0 ) >= self.STATE.buffer.shape[0]:
+                logger.warn( 'Sampling failed: insufficient buffer accumulation for requested sample period' )
+                return
+
+            self.STATE.triggers.append( TriggerInfo( msg = msg, offset = offset ) )
+
+        else: logger.warn( 'Sampling failed: no signal to sample yet' )
 
     @ez.subscriber( INPUT_SIGNAL )
     @ez.publisher( OUTPUT_SAMPLE )
@@ -73,52 +90,38 @@ class Sampler( ez.Unit ):
             msg_data.shape[1:] != last_msg_data.shape[1:]
         ):
             # Data stream changed meaningfully -- flush buffer, stop sampling
-            if self.STATE.trigger_info is not None:
-                logger.warn( 'Sampling failed: signal properties changed' )
-
+            if len( self.STATE.triggers ) > 0:
+                logger.warn( 'Sampling failed: Discarding all triggers' )
+            logger.warn( 'Flushing buffer: signal properties changed' )
             self.STATE.buffer = None
-            self.STATE.trigger_info = None
-            self.STATE.trigger_offset = None
-
-        # We will report a single sample at minimum
-        start_offset = int( self.SETTINGS.sample_per[0] * msg.fs )
-        stop_offset = int( self.SETTINGS.sample_per[1] * msg.fs )
-        max_buf_len: int = max( ( 0, -start_offset ) ) + 1
+            self.STATE.triggers = list()
 
         # Accumulate buffer ( time dim => dim 0 )
         self.STATE.buffer = msg_data if self.STATE.buffer is None else \
             np.concatenate( ( self.STATE.buffer, msg_data ), axis = 0 )
 
-        # trigger_offset points to t = 0 within buffer
-        if self.STATE.trigger_offset is not None:
-            self.STATE.trigger_offset -= msg.n_time
+        remaining_triggers: List[ TriggerInfo ] = list()
+        for trigger in self.STATE.triggers:
 
-        if self.STATE.trigger_info is not None:
-            start = self.STATE.trigger_offset + start_offset
-            stop = self.STATE.trigger_offset + stop_offset
+            # trigger_offset points to t = 0 within buffer
+            trigger.offset -= msg.n_time
+            start = trigger.offset + int( trigger.msg.period[0] * msg.fs )
+            stop = trigger.offset + int( trigger.msg.period[1] * msg.fs )
 
-            if stop < 0:
-                if abs( start ) < self.STATE.buffer.shape[0]:
+            if stop < 0: # We should be able to dispatch a sample
+                sample_data = self.STATE.buffer[ start : stop, ... ]
+                sample_data = np.swapaxes( sample_data, msg.time_dim, 0 )
+                
+                yield self.OUTPUT_SAMPLE, SampleMessage( 
+                    trigger = trigger.msg,
+                    sample = replace( msg, data = sample_data )
+                )
 
-                    # We should be able to dispatch a sample
-                    sample_data = self.STATE.buffer[ start : stop, ... ]
-                    sample_data = np.swapaxes( sample_data, msg.time_dim, 0 )
-                    
-                    yield self.OUTPUT_SAMPLE, SampleMessage( 
-                        trigger_info = self.STATE.trigger_info,
-                        sample = replace( msg, data = sample_data ),
-                        time_offset = start_offset / msg.fs
-                    )
+            else: remaining_triggers.append( trigger )
+        self.STATE.triggers = remaining_triggers
 
-                else: logger.warn( 'Sampling failed: insufficient buffer size' )
-
-                self.STATE.trigger_info = None
-                self.STATE.trigger_offset = None
-
-        # We only want to prune buffer if we aren't sampling
-        elif self.STATE.buffer.shape[ 0 ] > max_buf_len:
-            self.STATE.buffer = self.STATE.buffer[ -max_buf_len:, ... ]
-
+        buf_len = int( self.SETTINGS.buffer_dur * msg.fs )
+        self.STATE.buffer = self.STATE.buffer[ -buf_len:, ... ]
         self.STATE.last_msg = msg
 
 
@@ -138,32 +141,47 @@ class SampleFormatter( ez.Unit ):
     @ez.subscriber( INPUT )
     @ez.publisher( OUTPUT )
     async def format( self, msg: SampleMessage ) -> AsyncGenerator:
-        str_msg = f'Trigger: {msg.trigger_info.trigger}, '
+        str_msg = f'Trigger: {msg.trigger.value}, '
         str_msg += f'{msg.sample.n_time} samples @ {msg.sample.fs} Hz, '
         
         time_axis = np.arange(msg.sample.n_time) / msg.sample.fs
-        time_axis = time_axis + msg.time_offset
+        time_axis = time_axis + msg.trigger.period[0]
         str_msg += f'[{time_axis[0]},{time_axis[-1]})'
         
         yield self.OUTPUT, str_msg
 
+class TriggerGeneratorSettings( ez.Settings ):
+    period: Tuple[ float, float ] # sec
+    prewait: float = 0.5 # sec
+    publish_period: float = 5.0 # sec
+
 class TriggerGenerator( ez.Unit ):
 
-    OUTPUT_TRIGGER = ez.OutputStream( int )
+    SETTINGS: TriggerGeneratorSettings
+
+    OUTPUT_TRIGGER = ez.OutputStream( SampleTriggerMessage )
 
     @ez.publisher( OUTPUT_TRIGGER )
     async def generate( self ) -> AsyncGenerator:
-        await asyncio.sleep( 0.5 )
+        await asyncio.sleep( self.SETTINGS.prewait )
 
         output = 0
         while True:
-            yield self.OUTPUT_TRIGGER, output
-            await asyncio.sleep( 5.0 )
+            yield self.OUTPUT_TRIGGER, SampleTriggerMessage(
+                period = self.SETTINGS.period,
+                value = output
+            )
+
+            await asyncio.sleep( self.SETTINGS.publish_period )
             output += 1
+
+class SamplerTestSystemSettings( ez.Settings ):
+    sampler_settings: SamplerSettings
+    trigger_settings: TriggerGeneratorSettings
 
 class SamplerTestSystem( ez.System ):
 
-    SETTINGS: SamplerSettings
+    SETTINGS: SamplerTestSystemSettings
 
     OSC = Oscillator()
     SAMPLER = Sampler()
@@ -172,7 +190,8 @@ class SamplerTestSystem( ez.System ):
     DEBUG = DebugLog()
 
     def configure( self ) -> None:
-        self.SAMPLER.apply_settings( self.SETTINGS )
+        self.SAMPLER.apply_settings( self.SETTINGS.sampler_settings )
+        self.TRIGGER.apply_settings( self.SETTINGS.trigger_settings )
 
         self.OSC.apply_settings( OscillatorSettings(
             n_time = 2, # Number of samples to output per block
@@ -196,7 +215,17 @@ class SamplerTestSystem( ez.System ):
 
 if __name__ == '__main__':
 
-    settings = SamplerSettings( sample_per = ( -10.0, 10.0 ) )
+    settings = SamplerTestSystemSettings( 
+        sampler_settings = SamplerSettings(
+            buffer_dur = 5.0
+        ),
+        trigger_settings = TriggerGeneratorSettings(
+            period = ( 1.0, 2.0 ),
+            prewait = 0.5,
+            publish_period = 5.0
+        )
+    )
+
     system = SamplerTestSystem( settings )
 
     ez.run_system( system )
